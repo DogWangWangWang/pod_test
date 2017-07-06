03040b3fdc3065c8879d76b2721c4525b8eeb0bc
chenhao <chenhao1@example.com>
2017-07-06 15:43:47 +0800

    加点东西

---
+++ Pod_test/Pod_test/Pod_test.h
@@ -0,0 +1,16 @@
+//
+//  Pod_test.h
+//  Pod_test
+//
+//  Created by 陈浩 on 2017/7/6.
+//  Copyright © 2017年 陈浩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pod_test : NSObject
+
+
++(void)test;
+
+@end
---
+++ Pod_test/Pod_test/Pod_test.m
@@ -0,0 +1,19 @@
+//
+//  Pod_test.m
+//  Pod_test
+//
+//  Created by 陈浩 on 2017/7/6.
+//  Copyright © 2017年 陈浩. All rights reserved.
+//
+
+#import "Pod_test.h"
+
+@implementation Pod_test
+
++(void)test{
+    
+    NSLog(@"瞅啥");
+    
+}
+
+@end
